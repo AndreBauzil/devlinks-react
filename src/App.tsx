626ea5ab6bfdb7a6828bc54a797631831ef3e012
Andre Bauzil <andregbauzil@gmail.com>
2024-02-29 12:26:35 -0300

    feat: create base file for the app with all components and styling

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { ListItem } from "./Components/ListItem"
+import { Profile } from "./Components/Profile"
+import { SocialLink } from "./Components/SocialLink"
+import { Switch } from "./Components/Switch"
+
+import './styles/main.css'
+
+function App() {
+
+  return (
+    <div 
+      className="bg-fuchsia-950 w-full max-w-[588px] mt-14 mx-auto py-6"
+    >
+      <Profile 
+        imgPath="./assets/avatar.jpg"
+        profileId="@AndreBauzil" 
+      />
+
+      <Switch />
+
+      <ul>
+        <ListItem 
+          href="./assets/Currículo - Andre Bauzil.pdf"
+          content="Ver meu Currículo - PT"
+        />
+        <ListItem 
+          href="./assets/Resume - Andre Bauzil.pdf"
+          content="See my Resume - EN"
+        />
+        <ListItem 
+          href="https://github.com/AndreBauzil?tab=repositories"
+          content="Ver meus projetos"
+        />
+        <ListItem 
+          href="./pages/projects.tsx"
+          content="Ver meus projetos"
+        />
+      </ul>
+
+      <div id="social-links">
+        <SocialLink 
+          href="https://github.com/AndreBauzil"
+          icon="logo-github"
+        />
+        <SocialLink 
+          href="https://instagram.com/andre_bauzil"
+          icon="logo-instagram"
+        />
+        <SocialLink 
+          href="https://linkedin.com/in/andre-bauzil"
+          icon="logo-linkedin"
+        />
+
+        <footer>Made with ❤ by myself</footer>
+      </div>
+    </div>
+  )
+}
+
+export default App
