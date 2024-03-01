b97dfd4f3ca7987899d1deff44b50d1c51f6f8fb
Andre Bauzil <andregbauzil@gmail.com>
2024-03-01 12:03:42 -0300

    refactor: add some extra styling classes from tailwind

--- src/Components/ListItem/index.tsx
+++ src/Components/ListItem/index.tsx
@@ -5,6 +5,13 @@ interface Props {
 
 export function ListItem({ href, content }: Props) {
   return (
-    <li><a href={href}>{content}</a></li>
+    <li
+      className="border-solid border-[var(--text-color)] border rounded-lg bg-[var(--surface-color)] max-w-80 w-full h-fit"
+    >
+      <a
+        className="flex items-center content-center py-4 px-6 bg-[var(--surface-color)] border border-solid border-[var(--stroke-color)] rounded-lg backdrop-blur-sm font-medium no-underline transition-[background] duration-200 hover:bg-[var(--surface-color-hover)] hover:border-solid"
+        href={href}
+      >{content}</a>
+    </li>
   )
 }
