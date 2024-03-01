ffb171bc25a76273ae6d00b2c9e4863569500be0
Andre Bauzil <andregbauzil@gmail.com>
2024-03-01 12:03:52 -0300

    refactor: add some extra styling classes from tailwind

--- src/Components/Switch/index.tsx
+++ src/Components/Switch/index.tsx
@@ -11,11 +11,21 @@ export function Switch() {
     }
 
     return (
-        <div id="switch" onClick={ toggleMode }>
+        <div 
+            id="switch" 
+            className="w-16 relative my-1 mx-auto"
+            onClick={ toggleMode }
+        >
             <button
-                className='w-1'
+                className='
+                    w-8 h-8 bg-white bg-no-repeat bg-center border-0 absolute top-1/2 left-0 z-[1] translate-y-[-50%] cursor-pointer animate hover:outline-8 hover:outline hover:outline-[var(--hightlight-color)]
+                    dark:animate-[slide-in .4s forwards] 
+                '
             />
-            <span></span>
+            <span
+                className="block w-16 h-6 bg-[var(--surface-color)] border border-solid border-[var(--stroke-color)] rounded-full backdrop-blur-sm "
+            >
+            </span>
         </div>
     )
 }
