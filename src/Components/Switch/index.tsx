2c8e4e5edd5d6ec4ba08739b6914ed4ca7633a5c
AndreBauzil <Andregbauzil@gmail.com>
2024-03-05 17:38:07 -0300

    refactor: add new variable for colors on classes

--- src/Components/Switch/index.tsx
+++ src/Components/Switch/index.tsx
@@ -18,13 +18,13 @@ export function Switch() {
         >
             <button
                 className='
-                    w-8 h-8 bg-white bg-switch bg-no-repeat bg-center border-0 absolute top-1/2 left-0 z-[1] translate-y-[-50%] cursor-pointer animate 
-                    hover:outline-8 hover:outline hover:outline-[var(--hightlight-color)]
+                    w-8 h-8 bg-white bg-switch bg-no-repeat bg-center rounded-full border-0 absolute top-1/2 left-0 z-[1] translate-y-[-50%] 
+                    cursor-pointer animate hover:outline-8 hover:outline hover:outline-highlight
                     dark:animate-[slide-in] 
                 '
             />
             <span
-                className="block w-16 h-6 bg-[var(--surface-color)] border border-solid border-[var(--stroke-color)] rounded-full backdrop-blur-sm "
+                className="block w-16 h-6 bg-surface border border-solid border-stroke rounded-full backdrop-blur-sm"
             >
             </span>
         </div>
