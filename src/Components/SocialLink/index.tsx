fac43d750f2eb4c921a02409c8aa59ec91aac6e7
AndreBauzil <Andregbauzil@gmail.com>
2024-03-05 17:38:42 -0300

    refactor: change logic for socialLinks component (not working fine yet)

--- src/Components/SocialLink/index.tsx
+++ src/Components/SocialLink/index.tsx
@@ -1,14 +1,29 @@
 import { IonIcon } from "@ionic/react";
+import { logoGithub, logoLinkedin, logoInstagram } from "ionicons/icons";
 
 interface Props{
-  href: string;
-  icon: string;
+  href: string[];
+  icons: string[];
 }
 
-export function SocialLink({ href, icon }: Props) {
+export function SocialLink({ href, icons }: Props) {
+  const iconMap: { [key: string]: string } = {
+    logoGithub: logoGithub,
+    logoLinkedin: logoLinkedin,
+    logoInstagram: logoInstagram
+  }
+
   return (
-    <a href={href}>
-      <IonIcon icon={icon}></IonIcon>
-    </a>
+    href.map(link => {
+      return (
+        <a href={link}>
+          {icons.map(icon => {
+            return (
+              <IonIcon key={icon} icon={iconMap[icon]}></IonIcon>
+            )
+          })}
+        </a>
+      )
+    })
   )
 }
