ac2d71ac9818d7860966dfdc89b260dc1eead9ab
Andre Bauzil <andregbauzil@gmail.com>
2024-03-05 11:47:12 -0300

    fix: fix paths for images and add new styiling

--- src/Components/Switch/index.tsx
+++ src/Components/Switch/index.tsx
@@ -6,8 +6,8 @@ export function Switch() {
         const img = document.querySelector<HTMLImageElement>("#profile img")
 
         html.classList.contains("light") ?
-            img?.setAttribute("src", "./assets/avatar-light.jpg") :
-            img?.setAttribute("src", "./assets/avatar.jpg")
+            img?.setAttribute("src", "./assets/utils/avatar-light.jpg") :
+            img?.setAttribute("src", "./assets/utils/avatar.jpg")
     }
 
     return (
@@ -18,9 +18,9 @@ export function Switch() {
         >
             <button
                 className='
-                    w-8 h-8 bg-white bg-no-repeat bg-center border-0 absolute top-1/2 left-0 z-[1] translate-y-[-50%] cursor-pointer animate 
+                    w-8 h-8 bg-white bg-switch bg-no-repeat bg-center border-0 absolute top-1/2 left-0 z-[1] translate-y-[-50%] cursor-pointer animate 
                     hover:outline-8 hover:outline hover:outline-[var(--hightlight-color)]
-                    dark:animate-[slide-in .4s forwards] 
+                    dark:animate-[slide-in] 
                 '
             />
             <span
